61dec9f15f7d035c9f1ff285a14016346d74075c
BMigaliza <migaliza.lyza@gmail.com>
2015-09-08 22:37:29 +0000

    Max fuction running and compiling well hence the entire file is running efficiently

--- Tester.java
+++ Tester.java
@@ -20,6 +20,33 @@ public class Tester {
             System.out.println(n +" * " +i+ "=" +n*i);
         }
     }
+    /*
+    the function find the factorial value of the given number in the parameter
+    @param n is the parameter of the number entered to find the given factorial value
+    */
+    
+    public void fact(int n){
+       int result=1;
+       for(int i=n;n>0;n--){
+           result=result *i;
+       }
+       
+       System.out.println("The factorial value is : "+result);
+    }
+    
+    /*
+    the function max() finds the maximum value in a given array of numbers
+    @param numbers is an array of int
+    */
+    public void max(int [] numbers){
+        int max = numbers[0];
+        for(int i=0;i<numbers.length;i++){
+            if(max<numbers[i]){
+                max=numbers[i];
+            }
+        }
+        System.out.println("Maximum value is: "+ max);
+    }
     
     
     public static void main(String[] args){
@@ -28,6 +55,16 @@ public class Tester {
             tester1.table(Integer.parseInt(args[1]));
         }
         
+        else if(args[0].equals("fact")){
+            tester1.fact(Integer.parseInt(args[1]));
+        }
+        else if(args[0].equals("max")){
+            int []arrayInt=new int[args.length-1];
+                for(int i=1;i<args.length;i++){
+                    arrayInt[i-1] = Integer.parseInt(args[i]);
+                }
+                tester1.max(arrayInt);
+        }
  
         
     }
